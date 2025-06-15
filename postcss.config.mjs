21eeef7c06d9aa4feb0de368f55a675291fc4623
Joseph Cosmiano <130757505+MrRy125@users.noreply.github.com>
2025-06-15 15:52:32 +0800

    Add files via upload

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-/** @type {import('postcss').Config} */
-module.exports = {
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {}
-  }
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
